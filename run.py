110f6e3e13b383ed620e3bf569543d2bac750e3e
itssubas <itssubas@gmail.com>
2020-04-11 16:49:53 +0545

    gunicorn requirement

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ from src.app import create_app
 
 load_dotenv(find_dotenv())
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-	env_name = os.getenv('FLASK_ENV')
-	app = create_app(env_name)
+	port = os.getenv('PORT')
 	# run app
-	app.run()
+	app.run(host='0.0.0.0', port=port)
